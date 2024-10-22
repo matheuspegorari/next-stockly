b3ce743ace1d776cc2dce177a4a244ef460d8f64
Matheus Pegorari <pegorari42@gmail.com>
2024-10-21 22:00:47 -0300

    feat: add products table

---
+++ app/products/_components/table-columns.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { Badge } from "@/app/_components/ui/badge";
+import { Product } from "@prisma/client";
+import { ColumnDef } from "@tanstack/react-table";
+import { Circle } from "lucide-react";
+
+const getStatusLabel = (status: string) => {
+  if (status === "IN_STOCK") {
+    return "Em estoque";
+  }
+  return "Esgotado";
+};
+
+export const productTableColumns: ColumnDef<Product>[] = [
+  {
+    accessorKey: "name",
+    header: "Produto",
+  },
+  {
+    accessorKey: "price",
+    header: "Vlr. Unitário",
+  },
+  {
+    accessorKey: "stock",
+    header: "Estoque",
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+    cell: (row) => {
+      const product = row.row.original;
+      const label = getStatusLabel(product.status);
+      return (
+        <>
+          <Badge
+            variant={product.status === "IN_STOCK" ? "default" : "destructive"}
+            className="gap-2"
+          >
+            <Circle
+              className={
+                product.status === "IN_STOCK"
+                  ? "fill-primary-foreground"
+                  : "fill-destructive-foreground"
+              }
+              size={8}
+            />
+            {label}
+          </Badge>
+        </>
+      );
+    },
+  },
+];
