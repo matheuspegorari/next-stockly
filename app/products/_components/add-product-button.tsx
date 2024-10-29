cee5c1071d8e849c98481e6ab6d89470f4155fb7
Matheus Pegorari <pegorari42@gmail.com>
2024-10-29 00:07:47 -0300

    refactor: add UpsertProductDialogContent

--- app/products/_components/add-product-button.tsx
+++ app/products/_components/add-product-button.tsx
@@ -1,52 +1,12 @@
 "use client";
-import { createProduct } from "@/app/_actions/products/create-product/index";
-import {
-  CreateProductSchemaType,
-  createProductSchema,
-} from "@/app/_actions/products/create-product/schema";
 import { Button } from "@/app/_components/ui/button";
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/app/_components/ui/dialog";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/app/_components/ui/form";
-import { Input } from "@/app/_components/ui/input";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Loader2Icon, PlusIcon } from "lucide-react";
+import { Dialog, DialogTrigger } from "@/app/_components/ui/dialog";
+import { PlusIcon } from "lucide-react";
 import { useState } from "react";
-import { useForm } from "react-hook-form";
-import { NumericFormat } from "react-number-format";
+import UpsertProductDialogContent from "./upsert-dialog-content";
 
 const AddProductButton = () => {
   const [dialogIsOpen, setDialogIsOpen] = useState(false);
-  const form = useForm<CreateProductSchemaType>({
-    shouldUnregister: true,
-    resolver: zodResolver(createProductSchema),
-    defaultValues: { name: "", price: 0, stock: 1 },
-  });
-
-  const onSubmit = async (data: CreateProductSchemaType) => {
-    try {
-      await createProduct(data);
-      setDialogIsOpen(false);
-    } catch (error) {
-      console.error(error);
-    }
-    console.log(data);
-  };
 
   return (
     <Dialog open={dialogIsOpen} onOpenChange={setDialogIsOpen}>
@@ -56,87 +16,9 @@ const AddProductButton = () => {
           Novo Produto
         </Button>
       </DialogTrigger>
-      <DialogContent>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <DialogHeader>
-              <DialogTitle>Criar produto</DialogTitle>
-              <DialogDescription>
-                Insira as informações abaixo
-              </DialogDescription>
-            </DialogHeader>
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Nome do Produto:</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Digite o nome do produto" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="price"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Preço (R$):</FormLabel>
-                  <FormControl>
-                    <NumericFormat
-                      thousandSeparator="."
-                      decimalSeparator=","
-                      fixedDecimalScale
-                      decimalScale={2}
-                      prefix="R$ "
-                      allowNegative={false}
-                      customInput={Input}
-                      onValueChange={(values) => {
-                        field.onChange(values.floatValue);
-                      }}
-                      {...field}
-                      onChange={() => {}}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="stock"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Estoque</FormLabel>
-                  <FormControl>
-                    <Input
-                      type="number"
-                      placeholder="Digite o estoque do produto"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <DialogFooter>
-              <DialogClose asChild>
-                <Button variant="secondary" type="reset">
-                  Cancelar
-                </Button>
-              </DialogClose>
-              <Button type="submit" disabled={form.formState.isSubmitting}>
-                {form.formState.isSubmitting && (
-                  <Loader2Icon className="animate-spin" size={18} />
-                )}
-                Salvar
-              </Button>
-            </DialogFooter>
-          </form>
-        </Form>
-      </DialogContent>
+      <UpsertProductDialogContent
+        onSuccessfulSubmit={() => setDialogIsOpen(false)}
+      />
     </Dialog>
   );
 };
