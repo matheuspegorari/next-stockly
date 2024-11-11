144a05b9c82ad950299399bd5d0944107a883cf2
Matheus Pegorari <pegorari42@gmail.com>
2024-11-10 22:35:50 -0300

    refactor: simplify Prisma client initialization by removing custom extensions

--- app/_lib/prisma.ts
+++ app/_lib/prisma.ts
@@ -4,21 +4,7 @@ declare global {
   var cachedPrisma: ReturnType<typeof createPrismaClient>;
 }
 const createPrismaClient = () => {
-  return new PrismaClient().$extends({
-    result: {
-      product: {
-        status: {
-          needs: { stock: true },
-          compute(product) {
-            if (product.stock <= 0) {
-              return "OUT_OF_STOCK";
-            }
-            return "IN_STOCK";
-          },
-        },
-      },
-    },
-  });
+  return new PrismaClient();
 };
 let prisma: ReturnType<typeof createPrismaClient>;
 if (process.env.NODE_ENV === "production") {
