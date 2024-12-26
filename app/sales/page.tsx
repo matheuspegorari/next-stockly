d9263df71dae86be675246cb1fde84bef04d61cd
Matheus Pegorari <pegorari42@gmail.com>
2024-12-26 15:14:29 -0300

    refactor: replace getProducts with cachedGetProducts for improved performance

--- app/sales/page.tsx
+++ app/sales/page.tsx
@@ -1,13 +1,13 @@
 import { ComboboxOption } from "../_components/ui/combobox";
 import { DataTable } from "../_components/ui/data-table";
-import { getProducts } from "../_data-access/product/get-products";
+import { cachedGetProducts } from "../_data-access/product/get-products";
 import { getSales } from "../_data-access/sales/get-sales";
 import CreateSaleButton from "./_components/create-sale-button";
 import { saleTableColumns } from "./_components/table-columns";
 
 const SalesPage = async () => {
   const sales = await getSales();
-  const products = await getProducts();
+  const products = await cachedGetProducts();
   const productOptions: ComboboxOption[] = products.map(
     (product: { id: string; name: string }) => ({
       value: product.id,
